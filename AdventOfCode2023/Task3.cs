68ed2854bc1e21902ef9719bd4131a55dcc58adb
leonm <leon.maderic@gmail.com>
2023-12-07 14:29:00 +0100

    Add Task3

---
+++ AdventOfCode2023/Task3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2023
+{
+    internal class Task3
+    {
+        const string filename = "task3.txt";
+
+        public static void RunPart1()
+        {
+            string[] lines = File.ReadAllLines(filename);
+            foreach(string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+    }
+}
